abaecca0e4cd7336e48c8dc0087c19d6622de597
codingMIKUU <2571780492@qq.com>
2023-08-18 19:58:10 +0800

    implement comment action

--- services/comment_service.go
+++ services/comment_service.go
@@ -22,3 +22,34 @@ func (s *CommentService) GetVideoComment(video_id string) ([]models.Comment, err
 	config.DB.Where("video_id = ? AND deleted_at is NULL", uint(id)).Find(&Comments)
 	return *Comments, nil
 }
+
+// 往DB中插入相应的评论。另外to_user_id似乎没用到，所以没设置
+func (s *CommentService) CreateComment(video_id uint, content string, user_id uint) (models.Comment, error) {
+	if len(content) == 0 { //不允许空评论
+		return models.Comment{}, errors.New("插入空评论！")
+	}
+	comment := models.Comment{
+		Content: content,
+		VideoId: video_id,
+		UserId:  user_id,
+	}
+	config.DB.Create(&comment)
+	return comment, nil
+}
+
+// 根据相应的评论获取ID
+func (s *CommentService) GetCommentById(comment_id int64) models.Comment {
+	var comment models.Comment
+	config.DB.Where("id = ?", uint(comment_id)).First(&comment)
+	return comment
+}
+
+func (s *CommentService) DeleteCommentById(user_id uint, comment_id uint) error {
+	var comment models.Comment
+	comment = s.GetCommentById(int64(comment_id))
+	if comment.UserId == user_id {
+		config.DB.Where("id = ?", comment_id).Delete(&comment)
+		return nil
+	}
+	return errors.New("无权限删除该评论!")
+}
